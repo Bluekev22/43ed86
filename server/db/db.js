428edc34b57fdf652f8e92f93bdcc8105da0ccde
Kevin Shank <Kevin_Shank@yahoo.com>
2022-04-08 18:45:53 -0400

    Place database credentials into .env file

--- server/db/db.js
+++ server/db/db.js
@@ -1,8 +1,9 @@
 const Sequelize = require('sequelize')
+require('dotenv').config()
 
 const db = new Sequelize(
   process.env.DATABASE_URL ||
-    'postgres://postgres:password@localhost:5432/messenger',
+    `postgres://${process.env.REACT_APP_DATABASE_CREDENTIALS}@localhost:5432/messenger`,
   {
     logging: false,
   }

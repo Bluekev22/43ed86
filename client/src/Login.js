78650a4be637d6f87dfdee49b9aec824b4e15cba
Kevin Shank <Kevin_Shank@yahoo.com>
2022-04-11 23:15:34 -0400

    Create LinkContainer component

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,15 +1,9 @@
 import React, { useEffect } from 'react'
 import { useHistory } from 'react-router-dom'
-import {
-  Grid,
-  Box,
-  Typography,
-  FormControl,
-  TextField,
-} from '@material-ui/core'
+import { Grid, Typography, FormControl, TextField } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 import SideCard from './components/Login/SideCard'
-import LinkBtn from './components/Login/LinkButton'
+import LinkContainer from './components/Login/LinkContainer'
 import LoginButton from './components/Login/LoginButton'
 
 const useStyles = makeStyles((theme) => ({
@@ -36,22 +30,7 @@ const useStyles = makeStyles((theme) => ({
       margin: 'auto',
     },
   },
-  linkContainer: {
-    marginTop: '4vh',
-    alignItems: 'center',
-    [theme.breakpoints.up('sm')]: {
-      flexDirection: 'row',
-      justifyContent: 'flex-end',
-    },
-    [theme.breakpoints.down('sm')]: {
-      flexDirection: 'row',
-      justifyContent: 'center',
-    },
-    [theme.breakpoints.down('xs')]: {
-      flexDirection: 'column',
-      justifyContent: 'space-around',
-    },
-  },
+
   entireFormContainer: {
     flexDirection: 'column',
     alignItems: 'center',
@@ -62,26 +41,8 @@ const useStyles = makeStyles((theme) => ({
       justifyContent: 'center',
     },
   },
-  accountQuestion: {
-    fontSize: '14px',
-    marginTop: '6vh',
-    color: theme.palette.secondary.main,
-    [theme.breakpoints.up('sm')]: {
-      textAlign: 'end',
-      marginRight: '5.5vw',
-    },
-    [theme.breakpoints.down('sm')]: {
-      textAlign: 'end',
-      marginRight: '4vw',
-    },
-    [theme.breakpoints.down('xs')]: {
-      flexDirection: 'column',
-      justifyContent: 'space-around',
-      marginRight: '0vw',
-      marginTop: '8vh',
-    },
-  },
-  loginBox: {
+
+  loginContainer: {
     marginTop: '6vh',
     [theme.breakpoints.up('sm')]: {
       width: '390px',
@@ -164,16 +125,12 @@ const Login = ({ user, login }) => {
         xs={12}
         className={classes.entireFormContainer}
       >
-        <Grid container item className={classes.linkContainer}>
-          <Grid item xl={6} lg={5} md={6} sm={4} xs={12}>
-            <Typography className={classes.accountQuestion}>
-              Don't have an account?
-            </Typography>
-          </Grid>
-          <Grid item xl={4} lg={4} md={5} sm={4} xs={12}>
-            <LinkBtn href="/register" text="Create account" />
-          </Grid>
-        </Grid>
+        <LinkContainer
+          href="/register"
+          text="Create account"
+          question="Don't have an account?"
+        />
+
         <Grid
           container
           item
@@ -181,7 +138,7 @@ const Login = ({ user, login }) => {
           justifyContent="center"
           alignItems="center"
         >
-          <Grid item className={classes.loginBox}>
+          <Grid item className={classes.loginContainer}>
             <Typography className={classes.loginStatement}>
               Welcome back!
             </Typography>
---
+++ client/src/components/Login/LinkContainer.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import { Grid, Typography } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+import LinkButton from './LinkButton'
+
+const useStyles = makeStyles((theme) => ({
+  linkContainer: {
+    alignItems: 'center',
+    [theme.breakpoints.up('sm')]: {
+      flexDirection: 'row',
+      justifyContent: 'flex-end',
+    },
+    [theme.breakpoints.down('sm')]: {
+      marginTop: '4vh',
+      flexDirection: 'row',
+      justifyContent: 'center',
+    },
+    [theme.breakpoints.down('xs')]: {
+      flexDirection: 'column',
+      justifyContent: 'space-around',
+    },
+  },
+  accountQuestion: {
+    fontSize: '14px',
+    marginTop: '6vh',
+    color: theme.palette.secondary.main,
+    [theme.breakpoints.up('sm')]: {
+      textAlign: 'end',
+      marginRight: '5.5vw',
+    },
+    [theme.breakpoints.down('sm')]: {
+      textAlign: 'end',
+      marginRight: '4vw',
+    },
+    [theme.breakpoints.down('xs')]: {
+      flexDirection: 'column',
+      justifyContent: 'space-around',
+      marginRight: '0vw',
+      marginTop: '8vh',
+    },
+  },
+}))
+
+const LinkContainer = ({ href, text, question }) => {
+  const classes = useStyles()
+  return (
+    <Grid container item className={classes.linkContainer}>
+      <Grid item xl={6} lg={5} md={6} sm={4} xs={12}>
+        <Typography className={classes.accountQuestion}>{question}</Typography>
+      </Grid>
+      <Grid item xl={4} lg={4} md={5} sm={4} xs={12}>
+        <LinkButton href={href} text={text} />
+      </Grid>
+    </Grid>
+  )
+}
+
+export default LinkContainer

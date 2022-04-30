b53039bcd8e35e4141d54fc02aea6ff0ed916b1b
Kevin Shank <Kevin_Shank@yahoo.com>
2022-04-30 18:27:12 -0400

    Have db sync

--- server/db/db.js
+++ server/db/db.js
@@ -20,6 +20,9 @@ db.authenticate()
   .then(() => {
     console.log("Connection has been established successfully.");
   })
+  .then(() => {
+    db.sync({ force: false });
+  })
   .catch((err) => {
     console.error("Unable to connect to the database:", err);
   });

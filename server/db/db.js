be121a0584e82f461bc2e7b7b7926a6085173d36
Kevin Shank <Kevin_Shank@yahoo.com>
2022-04-30 18:12:42 -0400

    Add logging

--- server/db/db.js
+++ server/db/db.js
@@ -5,6 +5,7 @@ const db = new Sequelize(
   process.env.DATABASE_URL ||
     `postgres://${process.env.REACT_APP_DATABASE_CREDENTIALS}@localhost:5432/messenger`,
   {
+    logging: false,
     dialect: "postgres",
     dialectOptions: {
       ssl: {
